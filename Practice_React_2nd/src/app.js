5634045f4685f78ddbfeb5250f4da2ed6584113c
DOBBI <cocomong_94@naver.com>
2022-03-31 16:33:06 +0900

    아 처음부터 나눠서 할 걸

--- Practice_React_2nd/src/app.js
+++ Practice_React_2nd/src/app.js
@@ -1,45 +1,5 @@
 import { createDOM, createElement, render } from "./react";
 
-// const vdom = { // 가상돔 !
-//   tag: 'p',
-//   props: {},
-//   children: [
-//     {
-//       tag: 'h1',
-//       props: {},
-//       children: ["React 만들기"],
-//     },
-//     {
-//       tag: 'ul',
-//       props: {},
-//       children: [
-//         {
-//           tag: 'li',
-//           props: {
-//             style: "color:red",
-//           },
-//           children: ["첫 번째 아이템"]
-//         },
-//         {
-//           tag: 'li',
-//           props: {
-//             style: "color:blue",
-//           },
-//           children: ["두 번째 아이템"]
-//         },
-//         {
-//           tag: 'li',
-//           props: {
-//             style: "color:green",
-//           },
-//           children: ["세 번째 아이템"]
-//         },
-
-//       ]
-//     }
-//   ],
-// };
-
 const vdom = createElement('p', {},
   createElement('h1', {}, "React 만들기"),
   createElement('ul', {},
@@ -49,6 +9,15 @@ const vdom = createElement('p', {},
   )
 );
 
-console.log(vdom);
+const vdom2 = <p>
+  <h1>React 만들기</h1>
+  <ul>
+    <li style="color:red">첫 번째 아이템</li>
+    <li style="color:blue">첫 번째 아이템</li>
+    <li style="color:green">첫 번째 아이템</li>
+  </ul>
+</p>
 
-render(vdom, document.querySelector('#root'));
+console.log(vdom2);
+
+render(vdom2, document.querySelector('#root'));

ea69d95d0bd9775d2255a486160886f6f946f96a
DOBBI <cocomong_94@naver.com>
2022-04-20 18:30:51 +0900

    20220420_PraticeReact

--- Practice_React_4th/practic4th/src/App.js
+++ Practice_React_4th/practic4th/src/App.js
@@ -1,19 +1,52 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+`;
+
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    border-radius:100px;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
 `;
 
 const Box = styled.div`
-  background-color: teal;
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji}:hover {
+    font-size: 98px;
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <div></div>
-      <div></div>
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji>🤩</Emoji>
+      </Box>
+      <Emoji>🔥</Emoji>
+    </Wrapper>
   );
 }
 
